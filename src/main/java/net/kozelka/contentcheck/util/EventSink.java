848db50b3a6b6e3d8935e07c09092ad11b312965
Petr Kozelka <pkozelka@gmail.com>
2014-08-12 11:25:40 +0200

    implemented simple event sink, to minimize listener boilerplate

---
+++ src/main/java/net/kozelka/contentcheck/util/EventSink.java
@@ -0,0 +1,68 @@
+package net.kozelka.contentcheck.util;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>Simple event sink.</p>
+ * <p>Proxies given listener interface, by distributing all received events to all registered listeners.</p>
+ *
+ * @author Petr Kozelka
+ */
+public class EventSink<T> {
+    private final List<T> listeners = new ArrayList<T>();
+
+    /**
+     * The proxy object; each of its method calls all registered listeners.
+     * Listeners are called in the order of registration; any thrown exception stops further sending.
+     */
+    public final T fire;
+
+    /**
+     * Creates simple event sink for given listener class.
+     * @param listenerClass -
+     * @return event sink
+     */
+    public static <T> EventSink<T> create(Class<T> listenerClass) {
+        return create(listenerClass, EventSink.class.getClassLoader());
+    }
+
+    public static <T> EventSink<T> create(Class<T> listenerClass, ClassLoader classLoader) {
+        return new EventSink<T>(listenerClass, classLoader);
+    }
+
+    @SuppressWarnings("unchecked")
+    private EventSink(Class<T> listenerClass, ClassLoader classLoader) {
+        fire = (T) Proxy.newProxyInstance(classLoader, new Class[]{listenerClass}, handler);
+    }
+
+    /**
+     * Registers a listener. Does not check for duplicates.
+     * @param listener -
+     */
+    public void addListener(T listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * Removes a listener from the registry.
+     * @param listener -
+     */
+    public void removeListener(T listener) {
+        listeners.remove(listener);
+    }
+
+    private final InvocationHandler handler = new InvocationHandler() {
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            for (T listener : listeners) {
+                method.invoke(listener, args);
+                //todo swallow unchecked exceptions?
+            }
+            return null;
+        }
+    };
+}
---
+++ src/test/java/net/kozelka/contentcheck/util/EventSinkTest.java
@@ -0,0 +1,61 @@
+package net.kozelka.contentcheck.util;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventSinkTest {
+
+    private EventSink<MyEvents> myEvents;
+    private StringBuilder sb;
+
+    @Before
+    public void setup() {
+        sb = new StringBuilder();
+        myEvents = EventSink.create(MyEvents.class);
+        myEvents.addListener(new MyEvents() {
+            @Override
+            public void onString(String s) {
+                sb.append("<");
+                sb.append(s);
+                sb.append(">");
+            }
+
+            @Override
+            public void onEmpty() {
+                sb.append("()");
+            }
+        });
+
+        myEvents.addListener(new MyEvents() {
+            @Override
+            public void onString(String s) {
+                sb.append("[");
+                sb.append(s);
+                sb.append("]");
+            }
+
+            @Override
+            public void onEmpty() {
+                sb.append("{}");
+            }
+        });
+        myEvents.removeListener(null);
+    }
+
+    @Test
+    public void testListener() throws Exception {
+        myEvents.fire.onString("A");
+        myEvents.fire.onEmpty();
+        myEvents.fire.onString("bb");
+        myEvents.fire.onEmpty();
+        myEvents.fire.onString("C");
+        Assert.assertEquals("<A>[A](){}<bb>[bb](){}<C>[C]", sb.toString());
+    }
+
+    private static interface MyEvents {
+        void onString(String s);
+        void onEmpty();
+    }
+
+}
