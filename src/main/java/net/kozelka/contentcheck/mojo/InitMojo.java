79fdc3f02514159e80b056f0803c9156ae73cdb3
Petr Kozelka <pkozelka@gmail.com>
2015-09-24 23:29:41 +0200

    git commit -am 'fixed checkstyle problem'

--- src/main/java/net/kozelka/contentcheck/mojo/InitMojo.java
+++ src/main/java/net/kozelka/contentcheck/mojo/InitMojo.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import net.kozelka.contentcheck.conflict.api.ArchiveConflict;
 import net.kozelka.contentcheck.conflict.api.ClassConflictReport;
 import net.kozelka.contentcheck.conflict.impl.ClassConflictAnalyzer;
 import net.kozelka.contentcheck.conflict.impl.ClassConflictPrinter;
